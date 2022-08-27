4efe85675942ec5f263f9abc661b37485e98c3ec
t-bello7 <bello4aus@gmail.com>
2022-08-27 18:19:21 +0100

    - create vet table - create bridge table for many to many relationships - insert data into vet and bridge tables - write queries for many-to-many relationships

--- queries.sql
+++ queries.sql
@@ -64,6 +64,8 @@ SELECT species, MAX(weight_kg), MIN(weight_kg) from animals GROUP BY species;
 SELECT species,date_of_birth, AVG(escape_attempts) from animals GROUP BY species, date_of_birth HAVING date_of_birth BETWEEN '1/1/1990' and '1/1/2000';
 
 
+/* MILESTONE 4 */
+
 /* Query for  What animals belong to Melody Pond? */
 SELECT name AS animal_name, full_name FROM animals A JOIN owners O ON A.owner_id = O.id WHERE full_name = 'Melody Pond';
 /* Query for  List of all animals that are pokemon (their type is Pokemon). */
@@ -78,4 +80,44 @@ SELECT S.name AS animal_name, full_name FROM animals A JOIN owners O ON A.owner_
 SELECT name AS animal_name, full_name, escape_attempts FROM animals A JOIN owners O ON A.owner_id = O.id WHERE A.escape_attempts = 0 and full_name = 'Dean Winchester';
 /* Query for  Who owns the most animals? */
 SELECT  full_name, COUNT(full_name) AS number FROM animals A JOIN owners O ON A.owner_id = O.id GROUP BY full_name ORDER BY number DESC LIMIT 1;
+/* Query Who was the last animal seen by William Tatcher? visit, animal, vet*/
+SELECT date_of_visit,  A.name as animal_name FROM animals A JOIN visits V ON V.animal_id = A.id JOIN vets T ON V.vet_id = T.id GROUP BY A.name,T.name, date_of_visit HAVING T.name = 'William Tatcher' ORDER BY date_of_visit DESC LIMIT 1;
+/* Query How many different animals did Stephanie Mendez see? */
+SELECT COUNT(A.name) FROM animals A JOIN visits V ON V.animal_id = A.id JOIN vets T ON V.vet_id = T.id WHERE T.name = 'Stephanie Mendez';
+/* Query List all vets and their specialties, including vets with no specialties. */
+SELECT V.name as vet_name, S.name AS species_name FROM specializations N JOIN species S ON N.species_id = S.id RIGHT JOIN vets V ON N.species_id = V.id ;
+/* Query List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020. */
+SELECT A.name as animal_name, T.name AS vet_name, date_of_visit FROM animals A JOIN visits V ON V.animal_id = A.id JOIN vets T ON V.vet_id = T.id WHERE T.name = 'Stephanie Mendez' and date_of_visit BETWEEN '04/01/2020' AND '08/30/2020';
+/* Query What animal has the most visits to vets? */
+SELECT COUNT(A.name) as number_of_visits, A.name as animal_name FROM animals A JOIN visits V ON V.animal_id = A.id JOIN vets T ON V.vet_id = T.id GROUP BY A.name ORDER BY number_of_visits DESC LIMIT 1;
+/* Query Who was Maisy Smith's first visit? */
+SELECT A.name as animal_name, T.name AS vet_name, date_of_visit FROM animals A JOIN visits V ON V.animal_id = A.id JOIN vets T ON V.vet_id = T.id WHERE T.name = 'Maisy Smith' ORDER BY date_of_visit LIMIT 1;
+/* Query Details for most recent visit: animal information, vet information, and date of visit. */
+SELECT A.name as animal_name, T.name AS vet_name, date_of_visit FROM animals A JOIN visits V ON V.animal_id = A.id JOIN vets T ON V.vet_id = T.id ORDER BY date_of_visit DESC LIMIT 5;
+
+/* Query How many visits were with a vet that did not specialize in that animal's species? */
+SELECT  *
+FROM specializations S
+FULL JOIN visits V ON S.vet_id = V.id;
+-- Then count the number of null values in vet id 
+
+SELECT COUNT(RES.animal_type_id)
+FROM (
+SELECT v.id vets_id, a.id, a.species_id animal_type_id, VISIT.vet_id, VISIT.animal_id
+    FROM visits VISIT 
+    JOIN vets v ON VISIT.vet_id = v.id
+    JOIN animals a ON a.id = VISIT.animal_id
+) AS RES
+
+JOIN specializations spec ON spec.vet_id = RES.vets_id;
+WHERE RES.animal_type_id <> spec.vet_id;
+
+/* Query What specialty should Maisy Smith consider getting? Look for the species she gets the most. */
+SELECT COUNT(RES.species_id), RES.species_id, S.name as species_name
+FROM(
+    SELECT MAX(species_id), A.species_id, date_of_visit FROM animals A JOIN visits V ON V.animal_id = A.id JOIN vets T ON V.vet_id = T.id GROUP BY species_id, date_of_visit, T.name HAVING T.name = 'Maisy Smith'
+) AS RES 
+JOIN species S ON RES.species_id = S.id
+GROUP BY RES.species_id, S.name ORDER BY count DESC LIMIT 1;
+
 
