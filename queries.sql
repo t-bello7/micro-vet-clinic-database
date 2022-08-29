6e05ad5b1c77c18937018933fc57d23ddd4e9093
brainconnect93 <brainconnect0@gmail.com>
2022-08-29 21:52:56 +0100

    Add the code for data performance

--- queries.sql
+++ queries.sql
@@ -121,4 +121,8 @@ FROM(
 JOIN species S ON RES.species_id = S.id
 GROUP BY RES.species_id, S.name ORDER BY count DESC LIMIT 1;
 
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4;
 
+SELECT COUNT(*) FROM visits where animal_id = 4;
+SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
