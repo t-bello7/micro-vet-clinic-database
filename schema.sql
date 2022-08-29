905c19c3c80a0c5dbe97719509fa56fb01b7ef88
t-bello7 <bello4aus@gmail.com>
2022-08-29 15:08:54 +0100

    - add an email column to owners table - generate a lot of data into owners and visits table

--- schema.sql
+++ schema.sql
@@ -85,3 +85,6 @@ CREATE TABLE visits (
     FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE CASCADE,
     FOREIGN KEY (animal_id) REFERENCES animals(id) ON DELETE CASCADE
 );
+
+-- Add an email column to your owners table
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
