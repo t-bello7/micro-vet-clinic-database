6062636ce1bce7a9e7cefd310a7863fd6ed40e76
t-bello7 <bello4aus@gmail.com>
2022-10-19 16:46:05 +0100

    ensure entry data are the right data types

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ This repository includes files with plain SQL that can be used to recreate a dat
 
 👤 **Author1**
 
-- GitHub: [@tbello-7](https://github.com/tbello-7)
+- GitHub: [@tbello-7](httpgits://github.com/tbello-7)
 - Twitter: [@__tbello](https://twitter.com/__tbello)
 - LinkedIn: [Oluwatomisin Bello](https://linkedin.com/in/tbello7)
 - Portfolio: [tbello in Tech](https://tbello.tech)
--- data.sql
+++ data.sql
@@ -1,9 +1,9 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon',  TO_DATE('03/02/2020', 'DD/MM/YYYY'), 0, TRUE, 10.23 );
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', TO_DATE('15/11/2018', 'DD/MM/YYYY'), 2, TRUE, 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', TO_DATE('15/11/2018', 'DD/MM/YYYY'), 2, TRUE, 8.0);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', TO_DATE('07/01/2021', 'DD/MM/YYYY'), 1, FALSE, 15.04);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', TO_DATE('12/05/2017', 'DD/MM/YYYY'), 5, TRUE, 11 );
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', TO_DATE('12/05/2017', 'DD/MM/YYYY'), 5, TRUE, 11.0);
 
 
 /* Insert new data */
